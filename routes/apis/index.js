6685722823fb676c62541413a190dd9c02ad5065
june850713@gmail.com <june850713@gmail.com>
2022-07-20 15:44:04 +0800

    feat: add admin-services

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
 
 router.get('/restaurants', restController.getRestaurants)
+router.use('/admin', admin)
 
 module.exports = router
